f5e98098406dfadf1abc5b5a6782e144d03cb6d0
Seccastudio <seccastudio@gmail.com>
2025-04-13 02:44:55 +0700

    Fix: Routing dengan BrowserRouter dan hapus Router di App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import './App.css';
@@ -23,65 +22,59 @@ import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Navbar />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/kegiatan" element={<Kegiatan />} />
+        <Route path="/keuangan" element={<Keuangan />} />
+        <Route path="/artikel" element={<Artikel />} />
+        <Route path="/program" element={<ProgramUnggulan />} />
+        <Route path="/login" element={<Login />} />
 
-        <Routes>
-          {/* Untuk Umum */}
-          <Route path="/" element={<Home />} />
-          <Route path="/kegiatan" element={<Kegiatan />} />
-          <Route path="/keuangan" element={<Keuangan />} />
-          <Route path="/artikel" element={<Artikel />} />
-          <Route path="/program" element={<ProgramUnggulan />} />
-          <Route path="/login" element={<Login />} />
-
-          {/* Untuk Admin */}
-          <Route
-            path="/admin/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/admin/kegiatan"
-            element={
-              <PrivateRoute>
-                <ManageKegiatan />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/admin/keuangan"
-            element={
-              <PrivateRoute>
-                <ManageKeuangan />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/admin/artikel"
-            element={
-              <PrivateRoute>
-                <ManageArtikel />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/admin/program"
-            element={
-              <PrivateRoute>
-                <ManageProgram />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-
-        <Footer />
-      </Router>
-    </AuthProvider>
+        <Route
+          path="/admin/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin/kegiatan"
+          element={
+            <PrivateRoute>
+              <ManageKegiatan />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin/keuangan"
+          element={
+            <PrivateRoute>
+              <ManageKeuangan />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin/artikel"
+          element={
+            <PrivateRoute>
+              <ManageArtikel />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin/program"
+          element={
+            <PrivateRoute>
+              <ManageProgram />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
